f9dddd171851a255356021c5351c7d47f0a6d6cd
Alexis Fretez <alexis.fretes@gopycloud.com>
2022-08-30 17:01:08 -0400

    dd

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
- 
+ var valorTotal = 10;
 function definirValor(_valor) {
-    
-    document.querySelector('#totalDeCompra').innerHTML = 'Total de compra: $' + _valor;
-    document.querySelector('.paypal-btn-container').style.display = 'block';
-    document.getElementById("precio_item").value = _valor;
+    valorTotal = _valor;
+    document.querySelector('#totalDeCompra').innerHTML = 'Total de compra: $' + valorTotal;
+    // document.querySelector('.paypal-btn-container').style.display = 'block';
+    // document.getElementById("precio_item").value = _valor;
 }
+
+ paypal.Button.render({
+     env: 'sandbox' ,
+     client: {
+         sandbox: 'AY9LOzb0f1aU_5Xz7NkW-Bwz5JRrDdPV_6ossVtqzbPrqvjb3jAZc8jEp-lsv17Q0FlQfGqpLKs4GCkO',    
+     },
+     payment: function () {
+         return paypal.rest.payment.create(this.props.env, this.props.client, {
+             transactions: [{
+                 amount: {
+                     total: valorTotal,
+                     currency: 'USD'
+                 }
+             }]
+         });
+     },
+     onAuthorize: function (data, actions) {
+         
+     }
+ }, "#mi-boton-paypal")
+
 
