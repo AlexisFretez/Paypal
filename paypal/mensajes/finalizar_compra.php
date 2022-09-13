097cd4989ea5c7f68294eb53b1351819e38bb2d8
Alexis Fretez <alexis.fretes@gopycloud.com>
2022-09-13 10:36:16 -0400

    Subio 1

--- paypal/mensajes/finalizar_compra.php
+++ paypal/mensajes/finalizar_compra.php
@@ -1,36 +1,21 @@
 <?php
-
-
+use PayPal\Api\ApiContext;
 use PayPal\Api\Payment;
 use PayPal\Api\PaymentExecution;
 
+require __DIR__. './../bootstrap.php';
 
-require __DIR__ . './../bootstrap.php';
-
-
-
-if( isset( $_GET['aprobado']) &&  $_GET['aprobado'] === 'true' ){
-
-    $paymentId = $_GET['paymentId'];
-
-    $payment = Payment::get($paymentId, $apiContext);
-
+if (isset($_GET['aprobado']) && $_GET['aprobado'] == 'true') {
+    $paymenId = $_GET['paymentId'];
+    $payment = Payment::get($paymenId, $apiContenido);
 
     $execution = new PaymentExecution();
     $execution->setPayerId($_GET['PayerID']);
+    $payment->execute($execution, $apiContenido);
 
-    $payment->execute($execution, $apiContext);
-
-    /*
-    actualizar las bd
-    */
+    //Actualizar la BD 
 
     header("Location:../mensajes/compra_finalizada.php");
-
-
-
-
 }else{
-        header("Location:../mensajes/cancelado.php");
+    header("Location:../mensajes/cancelado.php");
 }
-?>
